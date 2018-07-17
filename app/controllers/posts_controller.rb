5826a80de93c6158d3b720b38737bda582c04feb
Vishal Sharma <vsh1819@gmail.com>
2018-07-17 09:32:53 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,8 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
+    
+    @tags = Tag.new
     @post = Post.new
   end
 
@@ -25,9 +27,12 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    
+    print @post.tags
 
     respond_to do |format|
       if @post.save
+        
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render action: 'show', status: :created, location: @post }
       else
@@ -69,6 +74,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name)
+      params.require(:post).permit(:name, :content, :tag_ids => [])
     end
 end
